598e24c666f3072e9948a3e254a4e3a27e286476
Depeshko Oleksandr <oleksandr.depeshko@existek.com>
2025-05-02 01:15:12 +0200

    add changes with first block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
